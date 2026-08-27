5687dc882845b544afe90e1a073f552e115ffd56
UlyssesGrijalva <grijalvaulysses@gmail.com>
2026-08-27 03:49:21 -0700

    Added emoticon to output

--- hello_world.cpp
+++ hello_world.cpp
@@ -8,6 +8,8 @@ using namespace std;
 int main() {
     // Will display Hello World! message in the terminal
     cout << "Hello World!" << endl;
+    // Displays a smiley face emoticon
+    cout << ":)" << endl;
 
     return 0;
 }
