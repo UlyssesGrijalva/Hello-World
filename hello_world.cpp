1617b32e344e9fbdf74860e61d4005350d066f92
UlyssesGrijalva <grijalvaulysses@gmail.com>
2026-08-27 03:45:53 -0700

    Updated output comment

--- hello_world.cpp
+++ hello_world.cpp
@@ -6,7 +6,7 @@ using namespace std;
  * This is a simple Hello World program in C++.
  */
 int main() {
-    // Print Hello World to the terminal
+    // Will display Hello World! message in the terminal
     cout << "Hello World!" << endl;
 
     return 0;
